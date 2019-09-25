58fe5a830b7ddecaf2612c3db1951660d22b3dcc
Mike Kytka <michael.kytka@kibocommerce.com>
2019-09-25 16:07:50 -0500

    Initial commit for SDK demo project. Added api clients for inventory and auth services.

---
+++ src/main/java/com/kibocommerce/sdkdemo/SdkdemoApplication.java
@@ -0,0 +1,98 @@
+package com.kibocommerce.sdkdemo;
+
+import com.kibocommerce.sdk.inventory.ApiException;
+import com.kibocommerce.sdk.inventory.api.InventoryControllerApi;
+import com.kibocommerce.sdk.inventory.api.ModifyInventoryControllerApi;
+import com.kibocommerce.sdk.inventory.model.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class SdkdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdkdemoApplication.class, args);
+    }
+
+    private InventoryControllerApi inventoryControllerApi;
+
+    private ModifyInventoryControllerApi modifyInventoryControllerApi;
+
+    @Value("${client.tenantID}")
+    private Integer tenantID;
+
+    @Value("${client.siteID}")
+    private Integer siteID;
+
+    @Value("${client.locationCode}")
+    private String locationCode;
+
+    @Value("${client.upc}")
+    private String upc;
+
+    public SdkdemoApplication(InventoryControllerApi inventoryControllerApi, ModifyInventoryControllerApi modifyInventoryControllerApi) {
+        this.inventoryControllerApi = inventoryControllerApi;
+        this.modifyInventoryControllerApi = modifyInventoryControllerApi;
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+
+            RefreshItem refreshItem = new RefreshItem();
+            refreshItem.setUpc(upc);
+            refreshItem.setQuantity(72);
+
+            RefreshRequest refreshRequest = new RefreshRequest();
+            refreshRequest.addItemsItem(refreshItem);
+
+            refreshRequest.setLocationCode(locationCode);
+
+            try {
+                //call refresh API to create inventory for item
+                var refreshResponse = modifyInventoryControllerApi.refresh(tenantID, refreshRequest);
+                System.out.println(refreshResponse);
+            }
+            catch (ApiException ex) {
+                System.out.println(ex.getResponseBody());
+            }
+
+            //let the inventory job complete
+            TimeUnit.SECONDS.sleep(2);
+
+            List<ItemQuantity> itemQuantities = new ArrayList<>();
+
+            ItemQuantity itemQuantity = new ItemQuantity();
+            itemQuantity.setUpc(upc);
+            itemQuantity.setQuantity(1);
+
+            itemQuantities.add(itemQuantity);
+
+            RequestLocation requestLocation = new RequestLocation();
+            requestLocation.setLocationCode(locationCode);
+            requestLocation.setUnit(null);
+
+            InventoryRequest inventoryRequest = new InventoryRequest();
+            inventoryRequest.setType(InventoryRequest.TypeEnum.ALL);
+            inventoryRequest.setItems(itemQuantities);
+            inventoryRequest.setRequestLocation(requestLocation);
+
+            try {
+                //get the inventory for the given upc at the specified location code
+                var inventoryResponse = inventoryControllerApi.postQueryInventory(tenantID, inventoryRequest, siteID);
+                System.out.println(inventoryResponse);
+            }
+            catch (ApiException ex) {
+                System.out.println(ex.getResponseBody());
+            }
+
+        };
+    }
+}
---
+++ src/main/java/com/kibocommerce/sdkdemo/config/ApiClientConfig.java
@@ -0,0 +1,99 @@
+package com.kibocommerce.sdkdemo.config;
+
+import com.kibocommerce.sdk.auth.ApiException;
+import com.kibocommerce.sdk.auth.api.AppAuthTicketsApi;
+import com.kibocommerce.sdk.auth.model.MozuAppDevContractsOAuthAccessTokenResponse;
+import com.kibocommerce.sdk.auth.model.MozuAppDevContractsOauthAuthRequest;
+import com.kibocommerce.sdk.inventory.ApiClient;
+import com.kibocommerce.sdk.inventory.api.InventoryControllerApi;
+import com.kibocommerce.sdk.inventory.api.ModifyInventoryControllerApi;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+public class ApiClientConfig {
+
+    @Value("${url.inventory}")
+    private String inventoryServiceUrl;
+
+    @Value("${url.auth}")
+    private String authServiceUrl;
+
+    @Value("${auth.clientId}")
+    private String clientID;
+
+    @Value("${auth.clientSecret}")
+    private String clientSecret;
+
+    @Value("${client.tenantID}")
+    private Integer tenantID;
+
+    @Value("${client.siteID}")
+    private Integer siteID;
+
+    @Bean
+    public com.kibocommerce.sdk.auth.ApiClient authApiClient() {
+        com.kibocommerce.sdk.auth.ApiClient authApiClient = new com.kibocommerce.sdk.auth.ApiClient();
+
+        authApiClient.setBasePath(authServiceUrl);
+        authApiClient.setReadTimeout(7000);
+
+        return authApiClient;
+    }
+
+    //auth api
+    @Bean
+    public AppAuthTicketsApi appAuthTicketsApi() {
+        return new AppAuthTicketsApi(authApiClient());
+    }
+
+    @Bean
+    public MozuAppDevContractsOAuthAccessTokenResponse authAccessToken() {
+
+        MozuAppDevContractsOauthAuthRequest mozuAppDevContractsOauthAuthRequest = new MozuAppDevContractsOauthAuthRequest();
+        mozuAppDevContractsOauthAuthRequest.clientId(clientID);
+        mozuAppDevContractsOauthAuthRequest.clientSecret(clientSecret);
+        mozuAppDevContractsOauthAuthRequest.setGrantType("client_credentials");
+
+        try {
+            MozuAppDevContractsOAuthAccessTokenResponse response = appAuthTicketsApi().oauthAuthenticateApp(tenantID, siteID, mozuAppDevContractsOauthAuthRequest);
+
+            if (response != null) {
+                return response;
+            }
+        }
+        catch (ApiException ex) {
+            System.out.println(ex.getResponseBody());
+        }
+
+        throw new RuntimeException("Could not acquire auth token");
+    }
+
+    @Bean
+    public ApiClient inventoryApiClient() {
+        ApiClient inventoryApiClient = new ApiClient();
+
+        inventoryApiClient.setBasePath(inventoryServiceUrl);
+        inventoryApiClient.setReadTimeout(7000);
+        inventoryApiClient.addDefaultHeader("Authorization", "Bearer " + authAccessToken().getAccessToken());
+
+        return inventoryApiClient;
+    }
+
+    //inventory APIs
+
+    @Bean
+    public InventoryControllerApi inventoryControllerApi() {
+        return new InventoryControllerApi(inventoryApiClient());
+    }
+
+    @Bean
+    public ModifyInventoryControllerApi modifyInventoryControllerApi() {
+        return new ModifyInventoryControllerApi(inventoryApiClient());
+    }
+
+
+}
